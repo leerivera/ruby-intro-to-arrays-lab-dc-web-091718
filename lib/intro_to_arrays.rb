262082685455f5dfc60a22eab090226a7319bcc8
leerivera80@gmail.com Rivera <leerivera80@gmail.com>
2018-08-25 04:30:44 +0000

    Done.

---
+++ lib/intro_to_arrays.rb
@@ -0,0 +1,30 @@
+def instantiate_new_array
+  @my_new_array = []
+  
+end
+
+def array_with_two_elements
+  @my_two_array = [1,2]
+end
+
+def first_element(array)
+  my_first_element = "Welcome to New York"
+end
+
+def third_element(array)
+  my_third_element = "Style"
+end
+
+def last_element(array)
+  my_last_element = "Out of The Woods"
+end
+def first_element_with_array_methods(array)
+  first_country = "Thailand"
+end
+def last_element_with_array_methods(array)
+  last_country = "Myanmar"
+end
+def length_of_array(array)
+  length = 8
+end
+  
